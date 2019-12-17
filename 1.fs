1b5203dc0ba2304a85c8706c765fa3cc738b0752
Ben Follington <5009316+bfollington@users.noreply.github.com>
2019-12-17 14:59:39 +1000

    Add Problem 1 description

--- 1.fs
+++ 1.fs
@@ -1,4 +1,10 @@
 module Problem1
+(*
+  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+  The sum of these multiples is 23.
+
+  Find the sum of all the multiples of 3 or 5 below 1000.
+*)
 
 let isMultipleOf factor value = value % factor = 0
 let isMultipleOf3Or5 value = isMultipleOf 3 value || isMultipleOf 5 value
